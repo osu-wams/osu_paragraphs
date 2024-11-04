0b08d2554ad4871b6cf29c0f3166c01c2914213c
Matthew Brabham <brabham.matthew@gmail.com>
2024-11-04 14:37:00 -0800

    Refactor vertical tabs JavaScript initialization
    Rewrote the JavaScript code to use Drupal behaviors and the once library to initialize vertical tabs. This ensures a cleaner integration with Drupal's JavaScript system and avoids potential issues with multiple initializations. Also, the click event handler now includes a console log for the target.

--- js/vertical-tabs.js
+++ js/vertical-tabs.js
@@ -2,22 +2,39 @@
  * @file
  * Custom JavaScript for vertical tabs.
  */
+(function ($, Drupal, once) {
+  Drupal.behaviors.osu_paragraphs_vertical_tabs = {
+    attach: function (context, settings) {
+      once('osu_paragraphs_vertical_tabs', 'div.paragraph.tab-wrapper-paragraph', context).forEach(
+        (element) => {
+          const tabLink = $(element).find('a.tab-wrapper-paragraph__tab-link');
+          tabLink.css('outline', '1px solid red');
 
-(function ($) {
-  $(document).ready(function () {
-    $('.vertical-tabs .tabs a').on('click', function (e) {
-      e.preventDefault();
-      let target = $(this).attr('href');
+          tabLink.on('click', function (e) {
+            e.preventDefault();
+            let target = $(this).attr('href');
+            console.log(target);
+          })
+        });
+    },
+  };
+})(jQuery, Drupal, once);
 
-      $('.vertical-tabs .tabs .tabs__item').removeClass('active');
-      $('.vertical-tabs .tabs .tabs__item .tabs__link').removeClass('tabs__link--active');
-      $(this).parent().addClass('active');
-      $(this).addClass('tabs__link--active');
-      $('.vertical-tabs .tabs-content .tabs-content__items').hide();
-      $(target).show();
-    });
-
-    // Initialize the first tab as active.
-    $('.vertical-tabs .tabs li:first-child a').click();
-  });
-})(jQuery);
+// (function ($) {
+//   $(document).ready(function () {
+//     $('.vertical-tabs .tabs a').on('click', function (e) {
+//       e.preventDefault();
+//       let target = $(this).attr('href');
+//
+//       $('.vertical-tabs .tabs .tabs__item').removeClass('active');
+//       $('.vertical-tabs .tabs .tabs__item .tabs__link').removeClass('tabs__link--active');
+//       $(this).parent().addClass('active');
+//       $(this).addClass('tabs__link--active');
+//       $('.vertical-tabs .tabs-content .tabs-content__items').hide();
+//       $(target).show();
+//     });
+//
+//     // Initialize the first tab as active.
+//     $('.vertical-tabs .tabs li:first-child a').click();
+//   });
+// })(jQuery, Drupal, once);
