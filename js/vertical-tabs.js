05cfc0c8ffa99188def3da7b3864f9f58ebde5d6
Matthew Brabham <brabham.matthew@gmail.com>
2024-11-04 16:54:16 -0800

    Refactor tab handling logic in vertical-tabs.js
    Streamline the click event listener to manage active states and content visibility. This refactor enhances the maintainability and readability of the code, ensuring a smoother user interaction with the vertical tabs.

--- js/vertical-tabs.js
+++ js/vertical-tabs.js
@@ -5,36 +5,36 @@
 (function ($, Drupal, once) {
   Drupal.behaviors.osu_paragraphs_vertical_tabs = {
     attach: function (context, settings) {
-      once('osu_paragraphs_vertical_tabs', 'div.paragraph.tab-wrapper-paragraph', context).forEach(
-        (element) => {
-          const tabLink = $(element).find('a.tab-wrapper-paragraph__tab-link');
-          tabLink.css('outline', '1px solid red');
+      once('osu_paragraphs_vertical_tabs', 'div.paragraph.tab-wrapper-paragraph', context).forEach((element) => {
+        const tabLink = $(element).find('a.tab-wrapper-paragraph__tab-link');
 
-          tabLink.on('click', function (e) {
-            e.preventDefault();
-            let target = $(this).attr('href');
-            console.log(target);
-          })
+        tabLink.on('click', function (e) {
+          e.preventDefault();
+          // Get all active elements and remove the active classes.
+          const allActiveTabs = $(element).find('.tab-wrapper-paragraph__tab.active');
+          const allActiveTabLinks = $(element).find('.tab-wrapper-paragraph__tab-link--active');
+          const allActiveTabContent = $(element).find('.tab-wrapper-paragraph__tab-content--active');
+          const clickedLInk = $(this);
+          const target = $(this).attr('href');
+          const parentTabs = $(this).closest('.tab-wrapper-paragraph__tabs');
+          $(target).css('max-height', '0');
+          $(parentTabs).css('height', '0');
+          allActiveTabs.removeClass('active');
+          allActiveTabLinks.removeClass('tab-wrapper-paragraph__tab-link--active');
+          allActiveTabContent.removeClass('tab-wrapper-paragraph__tab-content--active');
+          allActiveTabContent.attr('aria-hidden', 'true');
+
+          // Set them to active.
+          $(clickedLInk).parent().addClass('active');
+          $(clickedLInk).addClass('tab-wrapper-paragraph__tab-link--active');
+          $(target).addClass('tab-wrapper-paragraph__tab-content--active');
+          $(target).removeAttr('aria-hidden');
+          const tabContentHeight = $(target).prop('scrollHeight');
+          const parentTabHeight = $(parentTabs).prop('scrollHeight');
+          $(target).css('max-height', tabContentHeight + 'px');
+          $(parentTabs).css('height', parentTabHeight + 'px');
         });
+      });
     },
   };
 })(jQuery, Drupal, once);
-
-// (function ($) {
-//   $(document).ready(function () {
-//     $('.vertical-tabs .tabs a').on('click', function (e) {
-//       e.preventDefault();
-//       let target = $(this).attr('href');
-//
-//       $('.vertical-tabs .tabs .tabs__item').removeClass('active');
-//       $('.vertical-tabs .tabs .tabs__item .tabs__link').removeClass('tabs__link--active');
-//       $(this).parent().addClass('active');
-//       $(this).addClass('tabs__link--active');
-//       $('.vertical-tabs .tabs-content .tabs-content__items').hide();
-//       $(target).show();
-//     });
-//
-//     // Initialize the first tab as active.
-//     $('.vertical-tabs .tabs li:first-child a').click();
-//   });
-// })(jQuery, Drupal, once);
