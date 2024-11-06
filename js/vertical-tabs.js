da76e5581c77359b4ee81a2a4b1eb2b48245e7a2
Matthew Brabham <brabham.matthew@gmail.com>
2024-11-06 14:25:11 -0800

    Refactor vertical tabs JavaScript
    Refactor vertical tabs script for better readability and maintainability. Enhanced event handling, and improved class and height adjustments for active tab content. Updated variable names for clarity and consistency.

--- js/vertical-tabs.js
+++ js/vertical-tabs.js
@@ -1,39 +1,61 @@
 /**
  * @file
  * Custom JavaScript for vertical tabs.
+ * phpcs:ignoreFile
  */
 (function ($, Drupal, once) {
-  Drupal.behaviors.osu_paragraphs_vertical_tabs = {
+  Drupal.behaviors.osuParagraphsVerticalTabs = {
     attach: function (context, settings) {
-      once('osu_paragraphs_vertical_tabs', 'div.paragraph.tab-wrapper-paragraph', context).forEach((element) => {
-        const tabLink = $(element).find('a.tab-wrapper-paragraph__tab-link');
+      once('osu-paragraphs-vertical-tabs', 'div.paragraph.tab-wrapper-paragraph', context).forEach((tabWrapperElement) => {
 
-        tabLink.on('click', function (e) {
-          e.preventDefault();
-          // Get all active elements and remove the active classes.
-          const allActiveTabs = $(element).find('.tab-wrapper-paragraph__tab.active');
-          const allActiveTabLinks = $(element).find('.tab-wrapper-paragraph__tab-link--active');
-          const allActiveTabContent = $(element).find('.tab-wrapper-paragraph__tab-content--active');
-          const clickedLInk = $(this);
-          const target = $(this).attr('href');
-          const parentTabs = $(this).closest('.tab-wrapper-paragraph__tabs');
-          $(target).css('max-height', '0');
-          $(parentTabs).css('height', '0');
-          allActiveTabs.removeClass('active');
-          allActiveTabLinks.removeClass('tab-wrapper-paragraph__tab-link--active');
-          allActiveTabContent.removeClass('tab-wrapper-paragraph__tab-content--active');
-          allActiveTabContent.attr('aria-hidden', 'true');
+        const handleClick = (event) => {
+          event.preventDefault();
 
-          // Set them to active.
-          $(clickedLInk).parent().addClass('active');
-          $(clickedLInk).addClass('tab-wrapper-paragraph__tab-link--active');
-          $(target).addClass('tab-wrapper-paragraph__tab-content--active');
-          $(target).removeAttr('aria-hidden');
-          const tabContentHeight = $(target).prop('scrollHeight');
-          const parentTabHeight = $(parentTabs).prop('scrollHeight');
-          $(target).css('max-height', tabContentHeight + 'px');
-          $(parentTabs).css('height', parentTabHeight + 'px');
-        });
+          const $tabWrapper = $(tabWrapperElement);
+          const $clickedTabLink = $(event.currentTarget);
+          const targetSelector = $clickedTabLink.attr('href');
+          const $targetContent = $(targetSelector);
+
+          // Get the height of all the tabs.
+          const $allTabHeight = $('.tab-wrapper-paragraph__tab')
+            .toArray()
+            .reduce((accumulator, tab) => Math.ceil($(tab).outerHeight(true)) + accumulator, 0);
+
+          const $allActiveTabs = $tabWrapper.find('.tab-wrapper-paragraph__tab.active');
+          const $allActiveTabLinks = $tabWrapper.find('.tab-wrapper-paragraph__tab-link--active');
+          const $allActiveTabContents = $tabWrapper.find('.tab-wrapper-paragraph__tab-content--active');
+
+          // Remove active classes and aria-hidden attributes
+          $allActiveTabs.removeClass('active');
+          $allActiveTabLinks.removeClass('tab-wrapper-paragraph__tab-link--active');
+          $allActiveTabContents.removeClass('tab-wrapper-paragraph__tab-content--active').css('max-height', '').attr('aria-hidden', 'true');
+
+          // Add active classes and remove aria-hidden attribute
+          $clickedTabLink.parent().addClass('active');
+          $clickedTabLink.addClass('tab-wrapper-paragraph__tab-link--active');
+          $targetContent.addClass('tab-wrapper-paragraph__tab-content--active').removeAttr('aria-hidden');
+
+          // Adjust heights
+          $targetContent.css('max-height', $targetContent.prop('scrollHeight') + 'px');
+          // Set the height of the osu-tabs container to the height of the controlledPanel if window width is greater than 769px
+          if (window.innerWidth > 768) {
+            const container = document.querySelector(
+              '.tab-wrapper-paragraph__tabs',
+            );
+            const $targetTabContentHeight = $targetContent.prop('scrollHeight');
+            // Check to ensure
+            // that the new height is not smaller than the total tab height.
+            if ($allTabHeight > $targetTabContentHeight) {
+              container.style.height = $allTabHeight + 'px';
+            } else {
+              container.style.height = $targetTabContentHeight + 'px';
+            }
+          }
+
+        };
+
+        const $tabLinks = $(tabWrapperElement).find('a.tab-wrapper-paragraph__tab-link');
+        $tabLinks.on('click', handleClick);
       });
     },
   };
