dd76bfc870544a423e34eb92600aa2554a8f83a9
Matthew Brabham <brabham.matthew@gmail.com>
2024-11-08 11:55:27 -0800

    Add hash change handler and improve tab click handling
    Implemented a function to handle URL hash changes for tab clicks, ensuring smooth navigation when hash links are used. Provided JSDoc comments for better code documentation and clarified existing comments for readability.

--- js/vertical-tabs.js
+++ js/vertical-tabs.js
@@ -7,7 +7,13 @@
   Drupal.behaviors.osuParagraphsVerticalTabs = {
     attach: function (context, settings) {
       once('osu-paragraphs-vertical-tabs', 'div.paragraph.tab-wrapper-paragraph', context).forEach((tabWrapperElement) => {
-
+        /**
+         * Handles click events for tab links within a tab wrapper.
+         * Prevents the default action of the event, processes the tab click to show the
+         * corresponding tab content, and updates the active states and ARIA attributes.
+         *
+         * @param {Event} event - The click event fired by the tab link.
+         */
         const handleClick = (event) => {
           event.preventDefault();
 
@@ -38,7 +44,8 @@
 
           // Adjust heights
           $targetContent.css('max-height', $targetContent.prop('scrollHeight') + 'px');
-          // Set the height of the osu-tabs container to the height of the controlledPanel if window width is greater than 769px
+          // Set the height of the osu-tabs container to the height of the controlledPanel
+          // if window width is greater than 769 px.
           if (window.innerWidth > 768) {
             const $targetTabContentHeight = $targetContent.prop('scrollHeight');
             // Check to ensure
@@ -54,7 +61,39 @@
 
         const $tabLinks = $(tabWrapperElement).find('a.tab-wrapper-paragraph__tab-link');
         $tabLinks.on('click', handleClick);
+
       });
+      /**
+       * Handles changes in the URL hash and triggers click events based on the hash value.
+       * Parses the hash and determines if it relates to a tab link or tab content,
+       * then simulates a click event on the corresponding element.
+       *
+       * @return {void}
+       */
+      const handleHashChange = () => {
+        const hash = window.location.hash;
+        if (hash) {
+          const $tabArr = hash.split('-');
+          if ($tabArr[0] === '#tab') {
+            if ($tabArr[1] === 'link') {
+              $(`a[id="${hash.substring(1)}"]`).trigger('click');
+            } else if ($tabArr[1] === 'content') {
+              $(`a[href="${hash}"]`).trigger('click');
+            }
+          }
+        }
+      };
+      if (window.innerWidth > 768) {
+        // Find all first tabs and for each one click on it.
+        once('osu-paragraphs-vertical-tabs-first-tab', 'div.paragraph.tab-wrapper-paragraph', context).forEach((tabWrapperElement) => {
+          $(tabWrapperElement).find('a.tab-wrapper-paragraph__tab-link').first().trigger('click');
+        });
+      }
+      // If an anchor link is used, open the linked tab item.
+      handleHashChange();
+      // If we load on the same page, run the script again.
+      $(window).on('hashchange', handleHashChange);
+
     },
   };
 })(jQuery, Drupal, once);
